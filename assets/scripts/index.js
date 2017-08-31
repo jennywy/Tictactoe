41adb511315e66c0d3565f7935480371b2e88eb9
jennywy <jennywy@gmail.com>
2017-08-30 22:50:01 -0400

    Created JS and CSS files
    Created some new js functions for separation of concerns later and a css
    page to style the html. Linked the css to index.html

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,6 +7,38 @@ $(() => {
   setAPIOrigin(location, config)
 })
 
+$(() => {
+  // all the event listeners for click inside a div go here
+  $('#0').on('click', function () {
+    console.log('You clicked 0')
+  })
+  $('#1').on('click', function () {
+    console.log('You clicked 1')
+  })
+  $('#2').on('click', function () {
+    console.log('You clicked 2')
+  })
+  $('#3').on('click', function () {
+    console.log('You clicked 3')
+  })
+  $('#4').on('click', function () {
+    console.log('You clicked 4')
+  })
+  $('#5').on('click', function () {
+    console.log('You clicked 5')
+  })
+  $('#6').on('click', function () {
+    console.log('You clicked 6')
+  })
+  $('#7').on('click', function () {
+    console.log('You clicked 7')
+  })
+  $('#8').on('click', function () {
+    console.log('You clicked 8')
+  })
+// event listeners for sign functions
+})
+
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
