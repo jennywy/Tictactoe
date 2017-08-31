a3716fbd2d24039f4b1554f1bb69b5e8fe56c4c5
jennywy <jennywy@gmail.com>
2017-08-31 12:13:01 -0400

    Game Check Logic
    Added logic to check for a single match

--- assets/scripts/dummy.js
+++ assets/scripts/dummy.js
@@ -6,13 +6,11 @@ let playerXMoves = []
 let playerOMoves = []
 
 let playerArray = function (gameMoves) {
-  for (var i = 0; i < gameMoves.length; i++) {
+  for (let i = 0; i < gameMoves.length; i++) {
   if (gameMoves[i] % 2 === 1) {
-    // add the value of the move into a new array called player0Moves
-    playerOMoves[i] = gameMoves[i]
+    playerOMoves.push(gameMoves[i])
   } else {
-    // add the value of the move into a new array called playerYMoves
-    playerXMoves[i] = gameMoves[i]
+    playerXMoves.push(gameMoves[i])
   }
 }
 }
@@ -20,18 +18,33 @@ playerArray(gameMoves)
 console.log(playerXMoves)
 console.log(playerOMoves)
 
-// gotta remove the empty strings in the player arrays!
-
-
 // if the playerMoves array is larger than 3, check for a match
 // if some of the numbers match all any of the matchid elements...
+const match = [0, 1, 2]
+const user = [0, 2, 1, 4]
+const bad = [3, 1, 2]
+const wrong = bad.sort()
+const sorted = user.sort()
+const check = function (sortedset) {
+  for (let i = 0; i < match.length; i++) {
+    if (match[i] === sortedset[i]) {
+      return true
+} else {
+  return false
+}
+}
+}
+check(sorted)
+check(wrong)
+
+// if some of b === match
 
 const matchIds = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
 const checkForMatch = function (playerMoves) {
   if (playerMoves.length >= 3) {
-    //
-
+    //go through each item of the matchIds to check if
+for
   }
 }
 
