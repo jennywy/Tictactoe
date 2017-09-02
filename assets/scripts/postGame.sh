0f6a023faa52158b9895e632bc9e08cff8284a1d
jennywy <jennywy@gmail.com>
2017-09-02 15:56:18 -0400

    Adding to test grunt deploy

--- assets/scripts/postGame.sh
+++ assets/scripts/postGame.sh
@@ -1,4 +1,4 @@
-# TOKEN="BAhJIiU5NTdlMGY0NDcxYzJiMWZmMDdkZDQzNGQzNTFjN2I2NAY6BkVG--6fdf3ea66d9414ba76ffca99f04f4526c2f30ab8" sh assets/scripts/create.sh
+# TOKEN="BAhJIiUwOWI3NGE3OTE2OGUzOTRkNDJhOTA1ODIwM2MxMzgzNQY6BkVG--3357e213e4129df650d1ea50c807146124bfcb26" sh assets/scripts/postGame.sh
 
 API="${API_ORIGIN:-http://tic-tac-toe.wdibos.com}"
 URL_PATH="/games"
